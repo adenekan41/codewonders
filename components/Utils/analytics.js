5aa5b98893bd20cc0fbbd10412293c9e8ec585a6
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2019-11-27 00:08:04 +0100

    :zap: Added GA for tracking visits

---
+++ components/Utils/analytics.js
@@ -0,0 +1,21 @@
+import ReactGA from 'react-ga'
+
+export const initGA = () => {
+  console.log('GA init')
+  ReactGA.initialize('UA-128989337-1')
+}
+
+export const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+}
+export const logEvent = (category = '', action = '') => {
+  if (category && action) {
+    ReactGA.event({ category, action })
+  }
+}
+export const logException = (description = '', fatal = false) => {
+  if (description) {
+    ReactGA.exception({ description, fatal })
+  }
+}
