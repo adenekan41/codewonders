936e87bb374467535279fe187e7be2841e107f0f
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2019-11-28 04:13:35 +0100

    :sparkles: Added PWA Support

--- pages/_document.js
+++ pages/_document.js
@@ -27,10 +27,25 @@ export default class MyDocument extends Document {
             <meta name="description" content="Hi, I&#x27;m Adenekan Wonderful, Frontend Developer and Javascript Engineer."/>
             <meta name="og:description" content="Hi, I&#x27;m Adenekan Wonderful, Frontend Developer and Javascript Engineer."/>
             <meta name="twitter:card" content="summary_large_image"/>
+            <meta name='mobile-web-app-capable' content='yes' />
+            <meta name='msapplication-config' content='/icons/browserconfig.xml' />
             <meta name="twitter:site" content="@code_wonders"/>
             <meta name="twitter:image" content="https://i.ibb.co/jz5Qwx1/Group-7.png"/>
             <link rel='shortcut icon' href='/favicon.ico' />
-            <link rel='apple-touch-icon' sizes='180x180' href='/icon.png' />
+            <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+            <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+            <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+            <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+            <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+            <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+            <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+            <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+            <link rel="icon" type="image/png" sizes="192x192"  href="/icons/android-icon-192x192.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+            <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
             <link rel='icon' href='/favicon.ico' />
             <link rel='manifest' href='/manifest.json' />
             <meta name="og:title" content="Adenekan Wonderful"/>
