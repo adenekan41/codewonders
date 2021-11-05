a532ee79c86765d2e8f653f2f72bff52d614c90a
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2021-11-05 05:45:41 +0100

    (chore): added scoutbar data

---
+++ components/Layout/data.js
@@ -0,0 +1,105 @@
+/* eslint-disable import/prefer-default-export */
+
+import {
+  createScoutAction,
+  createScoutSection,
+  createScoutPage,
+} from 'scoutbar';
+import ARTICLE_DATA from '../AppData/articles';
+import LENS_DATA from '../AppData/lens';
+import PROJECTS_DATA from '../AppData/projects';
+
+export const actions = (theme) => [
+  createScoutAction({
+    label: 'Home',
+    href: '/',
+    keyboardShortcut: ['h'],
+  }),
+
+  createScoutAction({
+    label: 'Resume',
+    href: '/resume',
+    keyboardShortcut: ['r'],
+  }),
+  createScoutAction({
+    label: 'Contact Me',
+    href: '/contact',
+    keyboardShortcut: ['c'],
+  }),
+
+  createScoutSection({
+    label: 'About Adenekan Wonderful',
+
+    children: [
+      createScoutAction({
+        label: 'About Adenekan Wonderful',
+        href: '/about',
+        keyboardShortcut: ['a'],
+      }),
+      createScoutAction({
+        label: "Adenekan Wonderful's Reads",
+        href: '/about#reads',
+      }),
+      createScoutAction({
+        label: 'Playlist',
+        href: '/about#playlist',
+      }),
+      createScoutAction({
+        label: "Adenekan's Lens",
+        href: '/about#lens',
+      }),
+    ],
+  }),
+
+  createScoutSection({
+    label: 'BY Codewonders',
+
+    children: [
+      createScoutPage({
+        label: 'Projects',
+        children: PROJECTS_DATA.map((project) =>
+          createScoutAction({
+            label: project.title,
+            href: project.link,
+            description: project.description,
+            icon: project.imageUrl,
+          })
+        ),
+      }),
+      createScoutPage({
+        label: 'Lens',
+        children: LENS_DATA.map((project) =>
+          createScoutAction({
+            label: project.title,
+            href: project.link,
+            description: project.description,
+            icon: project.imageUrl,
+          })
+        ),
+      }),
+      createScoutPage({
+        label: 'Articles',
+        children: ARTICLE_DATA.map((project) =>
+          createScoutAction({
+            label: project.title,
+            href: project.link,
+            description: project.description,
+            icon: project.imageUrl,
+          })
+        ),
+      }),
+    ],
+  }),
+
+  createScoutSection({
+    label: 'Preference',
+
+    children: [
+      createScoutAction({
+        label: 'Change Theme',
+        action: () => theme(),
+        keyboardShortcut: ['meta', 'l'],
+      }),
+    ],
+  }),
+];
