5f3ec9f96046f6b71dfd15f1b71c367e05fe8ff8
Adenekan Wonderful <adenekanwonderful41@gmail.com>
2021-11-29 23:22:52 +0100

    (chore): updated next-pwa

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ module.exports = withPWA({
   },
   pwa: {
     dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
   },
   images: {
     domains: ['i.ibb.co'],
